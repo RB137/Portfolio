e9f2ad10c71e72fbc47c530c104e10982c0a83ee
RAMESWAR BISOYI <161145698+RB137@users.noreply.github.com>
2024-03-27 03:24:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text", {
+    strings: ['Frontend Developer','YouTuber','Web Developer','Android Developer'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDe1ay:1000,
+    loop:true
+});
+
